a4b4e5ecc61e020bc920cdae60d26be39c4051eb
meylisday <victory.sett@gmail.com>
2018-08-29 22:32:59 +0300

    init

---
+++ assets/index.js
@@ -0,0 +1,12 @@
+import './css/main.scss';
+
+import Swiper from 'swiper';
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+});
